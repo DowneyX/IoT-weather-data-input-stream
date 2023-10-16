0e07641c3cff5f349306fc7d864f76e668f47279
DowneyX <DowneyX@github.com>
2023-10-16 18:50:58 +0200

    deleted azure connection

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import paho.mqtt.client as mqtt
 import config
 import json
 from datetime import datetime
-from azure.iot.device import IoTHubDeviceClient
-from azure.iot.device.exceptions import ConnectionFailedError, ConnectionDroppedError, OperationTimeout, OperationCancelled, NoConnectionError
-from azure.iot.device import Message
 import requests
 
 #callback
@@ -23,21 +20,6 @@ def on_data_recieved(client, userdata, msg):
 def post_data_to_api(data, api):
     r = requests.post(api, data)
     print(f"Status Code: {r.status_code}, Response: {r.json()}")
-
-def send_data_to_iot_hub(device_client: IoTHubDeviceClient, message):
-    telemetry = Message(json.dumps(message))
-    telemetry.content_encoding = "utf-8"
-    telemetry.content_type = "application/json"
-    
-    try:
-        device_client.send_message(telemetry)
-    except (ConnectionFailedError, ConnectionDroppedError, OperationTimeout, OperationCancelled, NoConnectionError):
-        raise("Message failed to send, skipping")
-    else:
-        print("Message successfully sent!", message)
-
-device_client = IoTHubDeviceClient.create_from_connection_string(config.IOT_HUB_CONNECTION_STRING, connection_retry=False)
-device_client.connect()
 
 client = mqtt.Client('weather_data_client')
 client.message_callback_add(config.TOPIC_SUB, on_data_recieved)
--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-IOT_HUB_CONNECTION_STRING = 'HostName=douweHub.azure-devices.net;DeviceId=raspberry-downey;SharedAccessKey=jM9ujUHXgKdlXieLCCRW26moKVJJ1QI0ZdVEeCH2XU8='
-
-MODAL_THRESHOLD = 10
-
 MQTT_SERVER = 'downeypi.local'
 TOPIC_SUB = 'weather_data'
-
